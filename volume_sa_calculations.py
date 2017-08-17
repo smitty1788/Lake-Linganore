6d58855becf3f13a7281a3fe8315ee6676c4b0e8
Randy Smith <smithr@hood.edu>
2017-08-17 15:11:53 -0400

    Add files via upload

---
+++ volume_sa_calculations.py
@@ -0,0 +1,52 @@
+import arcpy
+
+arcpy.env.workspace = "G:\Lake Linganore"
+arcpy.env.overwriteOutput = True
+
+# Local Variables
+volume = 'Data\Tab\Volume.csv'
+LL_bathymetry = 'Data\Spatial Data\Depth\depth.gdb\IDW'
+
+# Create List of depths to calculate volume/SA
+# Depth ranges derived from min/max depths in Bathymetry raster 
+depths = []
+calc = 0
+
+# Creates range of depths from 0 to 38 
+while int(calc) <= 38:
+    depths.append(calc)
+    calc = int(calc) + 1
+
+# Calculate Volume/SA for depths 0 to 38
+for i in depths:
+	
+	arcpy.SurfaceVolume_3d(LL_bathymetry, volume, "ABOVE", str(i), "1", "0")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
